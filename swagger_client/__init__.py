905b4df6b9ea56e8bd89a8e8643bd70677b7699b
Ward, Michael S <Michael.Ward@pearson.com>
2016-01-28 07:24:26 -0700

    create v1beta1 python client

---
+++ swagger_client/__init__.py
@@ -0,0 +1,94 @@
+from __future__ import absolute_import
+
+# import models into sdk package
+from .models.v1beta1_http_ingress_rule_value import V1beta1HTTPIngressRuleValue
+from .models.v1beta1_subresource_reference import V1beta1SubresourceReference
+from .models.v1_tcp_socket_action import V1TCPSocketAction
+from .models.v1_env_var_source import V1EnvVarSource
+from .models.unversioned_list_meta import UnversionedListMeta
+from .models.v1beta1_ingress_spec import V1beta1IngressSpec
+from .models.v1_volume_mount import V1VolumeMount
+from .models.v1_capability import V1Capability
+from .models.v1beta1_deployment_strategy import V1beta1DeploymentStrategy
+from .models.v1beta1_ingress_status import V1beta1IngressStatus
+from .models.v1beta1_api_version import V1beta1APIVersion
+from .models.v1beta1_ingress_list import V1beta1IngressList
+from .models.v1beta1_rolling_update_deployment import V1beta1RollingUpdateDeployment
+from .models.v1_host_path_volume_source import V1HostPathVolumeSource
+from .models.v1_volume import V1Volume
+from .models.v1_empty_dir_volume_source import V1EmptyDirVolumeSource
+from .models.v1beta1_deployment_status import V1beta1DeploymentStatus
+from .models.v1beta1_cpu_target_utilization import V1beta1CPUTargetUtilization
+from .models.v1_http_get_action import V1HTTPGetAction
+from .models.v1_delete_options import V1DeleteOptions
+from .models.v1_pod_template_spec import V1PodTemplateSpec
+from .models.v1_probe import V1Probe
+from .models.v1_nfs_volume_source import V1NFSVolumeSource
+from .models.v1_ceph_fs_volume_source import V1CephFSVolumeSource
+from .models.v1_capabilities import V1Capabilities
+from .models.v1_iscsi_volume_source import V1ISCSIVolumeSource
+from .models.v1_load_balancer_status import V1LoadBalancerStatus
+from .models.v1beta1_ingress_backend import V1beta1IngressBackend
+from .models.unversioned_status import UnversionedStatus
+from .models.v1beta1_deployment_list import V1beta1DeploymentList
+from .models.v1_git_repo_volume_source import V1GitRepoVolumeSource
+from .models.v1_secret_volume_source import V1SecretVolumeSource
+from .models.unversioned_status_details import UnversionedStatusDetails
+from .models.v1_object_meta import V1ObjectMeta
+from .models.v1_resource_requirements import V1ResourceRequirements
+from .models.v1beta1_pod_selector import V1beta1PodSelector
+from .models.unversioned_patch import UnversionedPatch
+from .models.v1beta1_job_status import V1beta1JobStatus
+from .models.v1beta1_http_ingress_path import V1beta1HTTPIngressPath
+from .models.v1_local_object_reference import V1LocalObjectReference
+from .models.v1_container_port import V1ContainerPort
+from .models.v1_downward_api_volume_source import V1DownwardAPIVolumeSource
+from .models.v1_cinder_volume_source import V1CinderVolumeSource
+from .models.v1_se_linux_options import V1SELinuxOptions
+from .models.v1beta1_ingress_rule import V1beta1IngressRule
+from .models.v1beta1_job_list import V1beta1JobList
+from .models.v1beta1_pod_selector_requirement import V1beta1PodSelectorRequirement
+from .models.v1_exec_action import V1ExecAction
+from .models.v1beta1_horizontal_pod_autoscaler_spec import V1beta1HorizontalPodAutoscalerSpec
+from .models.v1_object_field_selector import V1ObjectFieldSelector
+from .models.v1_persistent_volume_claim_volume_source import V1PersistentVolumeClaimVolumeSource
+from .models.v1beta1_job_condition import V1beta1JobCondition
+from .models.v1_lifecycle import V1Lifecycle
+from .models.v1beta1_deployment_spec import V1beta1DeploymentSpec
+from .models.v1beta1_third_party_resource import V1beta1ThirdPartyResource
+from .models.json_watch_event import JsonWatchEvent
+from .models.v1_handler import V1Handler
+from .models.v1_aws_elastic_block_store_volume_source import V1AWSElasticBlockStoreVolumeSource
+from .models.v1beta1_ingress import V1beta1Ingress
+from .models.v1_fc_volume_source import V1FCVolumeSource
+from .models.v1beta1_scale_spec import V1beta1ScaleSpec
+from .models.v1_downward_api_volume_file import V1DownwardAPIVolumeFile
+from .models.v1beta1_horizontal_pod_autoscaler_status import V1beta1HorizontalPodAutoscalerStatus
+from .models.v1_container import V1Container
+from .models.v1_pod_spec import V1PodSpec
+from .models.v1_flocker_volume_source import V1FlockerVolumeSource
+from .models.v1_rbd_volume_source import V1RBDVolumeSource
+from .models.v1beta1_horizontal_pod_autoscaler_list import V1beta1HorizontalPodAutoscalerList
+from .models.v1_load_balancer_ingress import V1LoadBalancerIngress
+from .models.v1_env_var import V1EnvVar
+from .models.v1beta1_job_spec import V1beta1JobSpec
+from .models.unversioned_status_cause import UnversionedStatusCause
+from .models.v1beta1_third_party_resource_list import V1beta1ThirdPartyResourceList
+from .models.v1beta1_horizontal_pod_autoscaler import V1beta1HorizontalPodAutoscaler
+from .models.v1_security_context import V1SecurityContext
+from .models.v1beta1_scale import V1beta1Scale
+from .models.v1beta1_scale_status import V1beta1ScaleStatus
+from .models.v1beta1_job import V1beta1Job
+from .models.v1_gce_persistent_disk_volume_source import V1GCEPersistentDiskVolumeSource
+from .models.v1_glusterfs_volume_source import V1GlusterfsVolumeSource
+from .models.v1beta1_deployment import V1beta1Deployment
+
+# import apis into sdk package
+from .apis.apisextensionsvbeta_api import ApisextensionsvbetaApi
+
+# import ApiClient
+from .api_client import ApiClient
+
+from .configuration import Configuration
+
+configuration = Configuration()
